0ef0cf8c15420f5f880a6102b070e8e7983fc73c
Tasfiul <mostafatasfiul@gmail.com>
2025-05-19 23:22:40 +0600

    added comment add, delete, edit views.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),
+    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('comments/new/<int:post_pk>/', views.CommentCreateView.as_view(), name='comment_create'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,88 @@ class PostCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin, PermissionRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    permission_required = 'blog.change_post'
+    permission_denied_message = 'You do not have permission to edit this post.'
+    def get_success_url(self):
+        # Redirect to the post detail page after successful update
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+    
+    # Define the test for UserPassesTestMixin (object-level permission)
+    def test_func(self):
+        # Get the post object being viewed/edited
+        post = self.get_object()
+        # Return True if the logged-in user is the author of the post
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+    permission_required = 'blog.delete_post'
+
+    def test_func(self):
+        # Get the post object being viewed for deletion
+        post = self.get_object()
+        # Return True if the logged-in user is the author of the post
+        return self.request.user == post.author
+
+class CommentUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
+    permission_required = 'blog.change_comment'
+    permission_denied_message = 'You do not have permission to edit this comment.'
+
+    def get_success_url(self):
+        # Redirect to the post detail page after successful update
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        # Get the comment object being viewed/edited
+        comment = self.get_object()
+        # Return True if the logged-in user is the author of the comment
+        return self.request.user == comment.author
+
+class CommentDeleteView(LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+    permission_required = 'blog.delete_comment'
+
+    def get_success_url(self):
+        # Redirect to the post detail page after successful deletion
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.post.pk})
+    
+    def test_func(self):
+        # Get the comment object being viewed for deletion
+        comment = self.get_object()
+        # Return True if the logged-in user is the author of the comment
+        return self.request.user == comment.author
+
+class CommentCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'blog/comment_form.html'
+    permission_required = 'blog.add_comment'
+
+    def get_success_url(self):
+        # Redirect to the post detail page after successful creation
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.kwargs['post_pk']})
+    def form_valid(self, form):
+        # Set the post for the comment
+        form.instance.post_id = self.kwargs['post_pk']
+        # Set the author of the comment
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = Post.objects.get(pk=self.kwargs['post_pk'])
+        return context
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -24,7 +106,7 @@ class PostListView(ListView):
     paginate_by = 10
 
 class PostDetailView(DetailView):
-        model = Post
-        template_name = 'blog/post_detail.html'
-        context_object_name = 'post'
-# Create your views here.
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
